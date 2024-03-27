bd54bd0e9a3c65d177e226478bcd459d55c49a4e
Ime Essien <imeessienime@gmail.com>
2024-03-27 08:40:27 -0700

    6-main 6-peak 6-peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-"""
-this module contains a function that finds the peak in a list of unsorted integers
-"""
+""" Finds a peak inside a list """
+
 
 def find_peak(list_of_integers):
-    """function to find peak , uses binary search"""
-    i = 0
-    j = len(list_of_integers) - 1
-    while i <= j:
-        m = int((i + j)/2)
-        if list_of_integers[m] > list_of_integers[m+1]:
-            j = m
-        else:
-            i = m + 1
-    return list_of_integers[i]
+    if list_of_integers == []:
+        return None
+
+    length = len(list_of_integers)
+    mid = int(length / 2)
+    li = list_of_integers
+
+    if mid - 1 < 0 and mid + 1 >= length:
+        return li[mid]
+    elif mid - 1 < 0:
+        return li[mid] if li[mid] > li[mid + 1] else li[mid + 1]
+    elif mid + 1 >= length:
+        return li[mid] if li[mid] > li[mid - 1] else li[mid - 1]
+
+    if li[mid - 1] < li[mid] > li[mid + 1]:
+        return li[mid]
+
+    if li[mid + 1] > li[mid - 1]:
+        return find_peak(li[mid:])
+    return find_peak(li[:mid])
